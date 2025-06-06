8ef8da1dfa8ced9a93b1c5fc05a7d2cdffbaab4e
dipudangol2 <dipudangol2@gmail.com>
2025-06-06 22:58:16 +0545

    Integrate Sentry for error tracking

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { sentryReactRouter, type SentryReactRouterBuildOptions } from "@sentry/react-router";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ssr: {
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "dipu-dangol",
+  project: "travel-agency",
+  // An auth token is required for uploading source maps.
+  authToken: "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"
+  // ...
+};
+
+
+export default defineConfig(config => {
+  return {
+    plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), sentryReactRouter(sentryConfig, config)],
+    ssr: {
     noExternal: [/@syncfusion/]
   }
+  };
 });
+
